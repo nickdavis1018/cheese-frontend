7d551a2924166f8ae5400e96b0203bbdfefeab32
Nicholas Davis <nickdavis1018@gmail.com>
2021-11-16 14:29:23 -0800

    updated heroku-path

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,7 +6,7 @@ import Show from "../pages/Show";
 function Main(props) {
   const [cheese, setCheese] = useState(null);
 
-  const URL = "http://localhost:4000/cheese/";
+  const URL = "https://cheesedata.herokuapp.com/cheese";
 
   const getCheese = async () => {
     const response = await fetch(URL);
